5953a97409ba95a95f334a96b3575020a575fa8c
yj95228 <kei02119@naver.com>
2022-01-07 22:41:27 +0900

    add login function

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-const daysOfWeek = ["mon","tue","wed","thu","fri","sat"];
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-console.log(daysOfWeek);
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-daysOfWeek.push("sun");
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
 
-console.log(daysOfWeek);
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+// localStorage.removeItem(USERNAME_KEY);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
