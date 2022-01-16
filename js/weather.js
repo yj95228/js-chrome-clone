36d89d786a4019917855c878fa5ac721d4e12993
yj95228 <kei02119@naver.com>
2022-01-16 17:09:23 +0900

    add weather

---
+++ js/weather.js
@@ -0,0 +1,17 @@
+const API_KEY = "d308d51adf6d7ee602a1d3c53ddd0946";
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = data.weather[0].main;
+    });
+}
+function onGeoError(){
+    alert("no");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
